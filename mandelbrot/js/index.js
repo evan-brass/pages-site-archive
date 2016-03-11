6f5b7b083dc3308c91df52e369dc4eef7b339e3b
Evan <brassevan@gmail.com>
2016-03-10 22:10:39 -0800

    Major update to the mandelbrot

--- mandelbrot/js/index.js
+++ mandelbrot/js/index.js
@@ -1,6 +1,9 @@
+const
+maxTries = 30; // 10 for dev, 30 for release
+
 var canvas = document.getElementById('canvas');
-canvas.style.width = "900px";
-canvas.style.height = "450px";
+canvas.style.width = "100%";
+canvas.style.height = "100%";
 
 var context = canvas.getContext('2d');
 
@@ -13,52 +16,81 @@ canvas.width = width;
 canvas.height = height;
 
 var aspectRatio = width / height;
-var unitHeight = 2;
+var unitHeight = 4;
 var unitWidth = aspectRatio * unitHeight;
 var image = context.getImageData(0, 0, width, height);
 var data = image.data;
 
-function isClose(c1, c2) {
-	const
-	reallySmall = 0.0000001;
-
-	var diffReal = c1.re - c2.re;
-	var diffImg = c1.im - c2.im;
-	if (math.abs(diffReal) < reallySmall && math.abs(diffImg) < reallySmall) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-function isMandelbrot(c, previousAnswers, tries) {
+// Gradiented mandelbrot
+function mandelbrotFailIter(c, previousAnswers, tries) {
 	previousAnswers = previousAnswers || [];
 	tries = tries || 0;
-
 	if (math.abs(c) > 2) {
-		return false;
+		return tries;
 	}
 
 	if (previousAnswers.some(function(item) {
 		return _.isEqual(c, item);
 	})) {
-		return true;
+		return -1;
 	}
 
 	if (previousAnswers.length > 0
 			&& isClose(c, previousAnswers[previousAnswers.length - 1])) {
-		return true;
+		return -1;
 	}
 
 	++tries;
-	if (tries > 30) {
-		return true;
+	if (tries > maxTries) {
+		return -1;
 	}
 
 	previousAnswers.push(c);
-	return isMandelbrot(math.add(math.square(c), previousAnswers[0]),
+	return mandelbrotFailIter(math.add(math.square(c), previousAnswers[0]),
 			previousAnswers, tries);
+}
 
+function gradientColorAt(x, y) {
+	var a, b;
+	a = (x / width) * unitWidth - unitWidth / 2;
+	b = (y / height) * unitHeight - unitHeight / 2;
+
+	var failIteration = mandelbrotFailIter(math.complex(a, b));
+	var intensity;
+	if(failIteration != -1){
+		intensity = Math.floor(failIteration/maxTries * 255);
+	} else {
+		intensity = 0;
+	}
+	return {
+		r: intensity,
+		g: intensity,
+		b: intensity
+	};
+}
+
+function createGradientMandelbrot(py) {
+	py = py || 0;
+	// iterate through each pixel in the row
+	for (var px = 0; px < image.width; ++px) {
+		var color = gradientColorAt(px, py);
+		data[(py * image.width * 4) + (px * 4)] = color.r; // Red
+		data[(py * image.width * 4) + (px * 4) + 1] = color.g; // Green
+		data[(py * image.width * 4) + (px * 4) + 2] = color.b; // Blue
+		data[(py * image.width * 4) + (px * 4) + 3] = 255; // Alpha
+	}
+	context.putImageData(image, 0, 0);
+
+	if (py < image.height) {
+		setTimeout(createGradientMandelbrot.bind(this, ++py), 1);
+	}
+
+}
+
+
+// Plain Black and white
+function isMandelbrot(c) {
+	return (mandelbrotFailIter(c) == -1);
 }
 
 function colorAt(x, y) {
@@ -80,7 +112,7 @@ function colorAt(x, y) {
 	}
 }
 
-function createMandelbrot(py, px) {
+function createMandelbrot(py) {
 	py = py || 0;
 	// iterate through each pixel in the row
 	for (var px = 0; px < image.width; ++px) {
@@ -91,19 +123,35 @@ function createMandelbrot(py, px) {
 		data[(py * image.width * 4) + (px * 4) + 3] = 255; // Alpha
 	}
 	context.putImageData(image, 0, 0);
-	
-	if(py < image.height){
-		setTimeout(createMandelbrot.bind(this, ++py), 10);
+
+	if (py < image.height) {
+		setTimeout(createMandelbrot.bind(this, ++py), 1);
 	}
 }
 
-function makeSolidBlack() {
+// Common functions
+function setBackground() {
 	for (var i = 0; i < data.length; i += 4) {
-		data[i] = 0; // red
-		data[i + 1] = 0; // green
-		data[i + 2] = 0; // blue
+		data[i] = 229; // red
+		data[i + 1] = 75; // green
+		data[i + 2] = 75; // blue
 		data[i + 3] = 255; // transparency
 	}
 }
-makeSolidBlack();
-createMandelbrot();
+
+
+function isClose(c1, c2) {
+	const
+	reallySmall = 0.0000001;
+
+	var diffReal = c1.re - c2.re;
+	var diffImg = c1.im - c2.im;
+	if (math.abs(diffReal) < reallySmall && math.abs(diffImg) < reallySmall) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+setBackground();
+createGradientMandelbrot();
