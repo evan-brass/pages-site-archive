5fe7b30f0623a83ff999ac42f0f50f9b5cbb054e
Evan Brass <Evan Brass>
2017-03-24 16:46:52 -0700

    Victoria

--- victoria/script.js
+++ victoria/script.js
@@ -7,10 +7,26 @@
     })
     img.src = "../imgs/1.jpg"
   })).then((img) => {
+      // Get the image's data
     	const canvas = document.createElement('canvas')
     	const ctx = canvas.getContext('2d')
       ctx.drawImage(img, 0, 0)
       const imgData = ctx.getImageData(0, 0, img.naturalWidth, img.naturalHeight)
       console.log(imgData)
+
+      // Make the dots
+      const pixelPerDot = 40
+      const numRows = screen.height / pixelsPerDot
+      const numColumns = screen.width / pixelsPerDot
+
+      const container = document.createElement('div')
+      for (let row = 0; row < numRows; ++row) {
+        for (let column = 0; column < numColumns; ++columns) {
+          let dot = document.createElement('div')
+          dot.className = "dot"
+          container.appendChild(dot)
+        }
+      }
+      document.body.appendChild(container)
   })
 })()
